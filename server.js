3c0098919aa183ebcda5e859662de9510968bfd4
Noah Rhodes <noahmrhodes@gmail.com>
2019-11-02 10:20:05 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//load environment variable from .env
+require('dotenv').config();
+
+//declar application dependencies
+const express = require('express');
+const cors = require('cors');
